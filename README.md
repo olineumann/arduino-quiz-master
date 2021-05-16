d1e29891c3c72b4db5c78444f5177d7029d208bd
Oliver Neumann <oliver.neumann@kit.edu>
2021-05-16 14:09:45 +0200

    Initial implementation of arduino buzzer program.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Quiz Master Game for Arduino
+
+Use an arduino board with switches as a buzzer and use the python GUI as a visual interface.
---
+++ src/arduino/buzzer/buzzer.ino
@@ -0,0 +1,54 @@
+const int BUZZER[] = {
+  2, 3, 4, 5, 6, 7, 8, 9
+};
+const int LED_PIN = 13;
+
+int number_of_buzzer = 8;
+bool buzzer_active[] = {
+  true, true, true, true, true, true, true, true
+};
+bool locked = false;
+
+
+void setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < number_of_buzzer; i++) {
+    pinMode(BUZZER[i], INPUT);
+  }
+  pinMode(LED_PIN, OUTPUT);
+}
+
+
+void loop() {
+  // check if buzzer is pressed and print information to serial interface
+  for (int i = 0; i < number_of_buzzer; i++) {
+    if (digitalRead(BUZZER[i]) && buzzer_active[i]) {
+      Serial.print(i);
+      Serial.print(";");
+      Serial.print(BUZZER[i]);
+      Serial.print(";");
+      Serial.println(millis());
+      buzzer_active[i] = false;
+      locked = true;
+    }
+  }
+
+  // display on arduino that someone has already buzzered
+  if (locked) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
+
+  if (Serial.available()) {
+    String input_string = Serial.readString();
+    if (input_string == "reset\n") {
+      locked = false;
+      for (int i = 0; i < number_of_buzzer; i++) {
+        buzzer_active[i] = true;
+      }
+    }
+  }
+
+  delay(10);
+}
